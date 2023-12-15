00b87c360928c6aa0ef0dc3f21ee86448130484e
mike <m.kudryavtsev@adm.tver.ru>
2023-12-15 12:11:10 +0300

    add organization functionals

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -2,4 +2,7 @@ class Organization < ApplicationRecord
   has_many :assets
   has_one :department, dependent: :destroy
   has_many :titles, dependent: :destroy
+  has_many :employees
+
+  default_scope { order(:name) }
 end
