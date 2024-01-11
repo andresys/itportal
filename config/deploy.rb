c70f155295771104cc5635c62cb9fba6d3fe1fc3
mike <m.kudryavtsev@adm.tver.ru>
2024-01-11 10:45:47 +0300

    add sidekiq service

--- config/deploy.rb
+++ config/deploy.rb
@@ -25,7 +25,7 @@ set :rvm_use_path, '/etc/profile.d/rvm.sh'
 # Some plugins already add folders to shared_dirs like `mina/rails` add `public/assets`, `vendor/bundle` and many more
 # run `mina -d` to see all folders and files already included in `shared_dirs` and `shared_files`
 # set :shared_dirs, fetch(:shared_dirs, []).push('public/assets')
-set :shared_files, fetch(:shared_files, []).push('config/database.yml', 'config/secrets.yml')
+set :shared_files, fetch(:shared_files, []).push('config/database.yml', 'config/secrets.yml', 'config/http1c.yml', 'config/phonebook.yml')
 set :shared_dirs, fetch(:shared_dirs, []).push('public/packs', 'node_modules', 'storage')
 
 # This task is the environment that is loaded for all remote run commands, such as
@@ -81,6 +81,7 @@ task :deploy do
 
     on :launch do
       command "sudo systemctl restart #{fetch(:user)}"
+      command "sudo systemctl restart #{fetch(:user)}-sidekiq"
     end
   end
 
